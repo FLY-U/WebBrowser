cfb789b05acfaf3bd10eb5c67927d0185ebf2501
FLY_U <zhanghao97hao@163.com>
2018-05-31 13:16:20 +0800

    init

---
+++ app/src/main/java/com/example/webbrowser/ActivityAbout.java
@@ -0,0 +1,17 @@
+package com.example.webbrowser;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class ActivityAbout extends Activity {
+    /** Called when the activity is first created. */
+    ImageView imageview1;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);;
+        setContentView(R.layout.about_activity);
+        imageview1=(ImageView)findViewById(R.id.photo1);
+        setTitle("This apk is made by ZhengHaibo");
+    }
+}
